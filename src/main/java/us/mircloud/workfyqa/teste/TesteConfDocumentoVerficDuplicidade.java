f9fa6e3ba5216fcb38ec1b137e21254a82a270ad
wellington.carvalho <wellingtondevops@gmail.com>
2018-05-18 16:49:25 -0300

    comecei classe Configura unicidade

--- src/main/java/us/mircloud/workfyqa/teste/TesteConfDocumentoVerficDuplicidade.java
+++ src/main/java/us/mircloud/workfyqa/teste/TesteConfDocumentoVerficDuplicidade.java
@@ -6,45 +6,62 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import us.mircloud.workfyqa.core.DSL;
+import us.mircloud.workfyqa.page.ConfiguracaoDocumentoPage;
+import us.mircloud.workfyqa.page.DashboardPage;
 import us.mircloud.workfyqa.page.LoginPage;
 
 public class TesteConfDocumentoVerficDuplicidade {
 	
 	private WebDriver driver;
-	private DSL dsl;
+	private DSL dsl;	
 	private LoginPage page;
+	private DashboardPage dash;
+	private ConfiguracaoDocumentoPage  confdocto;
 
 	@Before
 	public void inicializa() {
 
-		System.setProperty("webdriver.chrome.driver","C:/webdrivers/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("http://workfy-qa.mircloud.us");
-		dsl = new DSL(driver);
+		new DSL(driver);
 		page = new LoginPage(driver);
+		dash= new DashboardPage(driver);
+		confdocto = new ConfiguracaoDocumentoPage(driver);	
+		
 	}
-	
-	@After
-	public void finaliza() {
 
-		driver.quit();
-	}
+
+	
+	//@After
+	//public void finaliza() {
+
+		//driver.quit();
+	//}
 	@Test
 	public void testeConfigurarDuplicidade() throws Exception {
 		
 		page.setEmail("wellington.carvalho@smartscan.com.br");
+
 		page.setPassowrd("123456");
+
 		page.setBotaoLogin();
-	
-		dsl.chekarComponente("Pesquisar Documentos", "//div[@id='search-main']/div/div/h1");
 
-		dsl.click_xpath("//div[@id='site-navbar-collapse']/ul/li/button/i/span");
+		dash.verificaTextoDocumentos();
 
-		dsl.chekarComponente("Configurações", "//div[@id='withBtnGroup']/button/span");
+		dash.setGerenciarDocumentos();
 
-		dsl.click_xpath("(//button[@type='button'])[20]");
-	
+		confdocto.verificaBotaoConfiguracoes();
+
+		confdocto.setDocumentoAvaliacaoDesempenho();
+
+		confdocto.checkTextoBotaoConfiguracao();
+
+		confdocto.setBotaoConfigDocumento();	
+
+		confdocto.obterTextoNomeDocumento();		
+
+		confdocto.obterTextoConfDentroConf();
 	
 	
 	}
